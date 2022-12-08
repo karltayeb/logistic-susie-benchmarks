c0986aa889b7a45ea82b877d2ec1135ab6ca25f6
Karl Tayeb <karl.tayeb7@gmail.com>
2022-12-08 14:23:46 -0600

    rename

--- R/website.R
+++ R/website.R
@@ -4,6 +4,6 @@
 website <- list(
   tar_render(web_linear_fail, 'notebooks/linear_fail.Rmd', output_dir='docs'),
   tar_render(web_home, 'notebooks/index.Rmd', output_dir='docs'),
-  tar_render(web_calibration, 'notebooks/calibration.Rmd', output_dir='docs'),
-  tar_render(web_calibration2, 'notebooks/calibration_constant.Rmd', output_dir='docs')
+  tar_render(web_constant_sim, 'notebooks/constant_sim_results.Rmd', output_dir='docs'),
+  tar_render(web_half_normal_sim, 'notebooks/half_normal_sim_results.Rmd', output_dir='docs')
 )
