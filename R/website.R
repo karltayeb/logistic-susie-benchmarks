cc77688c169a46ca63a219ec1e7863598529233d
Karl Tayeb <karl.tayeb7@gmail.com>
2023-01-12 07:56:06 -0600

    add yusha's example

--- R/website.R
+++ R/website.R
@@ -5,11 +5,6 @@ website <- list(
   tar_render(web_linear_fail, 'notebooks/linear_fail.Rmd', output_dir='docs'),
   tar_render(web_home, 'notebooks/index.Rmd', output_dir='docs'),
   tar_render(web_constant_sim, 'notebooks/constant_sim_results.Rmd', output_dir='docs'),
-  tar_render(web_half_normal_sim, 'notebooks/half_normal_sim_results.Rmd', output_dir='docs')
+  tar_render(web_half_normal_sim, 'notebooks/half_normal_sim_results.Rmd', output_dir='docs'),
+  tar_render(web_yusha_example, 'notebooks/yusha_sc_tumor.Rmd', output_dir='docs')
 )
-
-
-library(dplyr)
-tar_meta() %>%
-  filter(stringr::str_detect(name, 'constant_fit')) %>%
-  head()
