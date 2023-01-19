4cc864a0a915af8c91785f2e36dd2c7d7d3ea3d1
Karl Tayeb <karl.tayeb7@gmail.com>
2023-01-19 09:44:34 -0600

    update website

--- _targets.R
+++ _targets.R
@@ -5,8 +5,12 @@ library(targets)
 library(tarchetypes) # Load other packages as needed. # nolint
 
 library(future)
+
 library(future.callr)
 plan(callr)
+
+# library(future.batchtools)
+# plan(batchtools_slurm, resources=list(account='pi-mstephens', time='1:0:0'))
 
 # Set target options:
 tar_option_set(
@@ -23,6 +27,7 @@ list(
   make_X_targets(),
   half_normal_target,
   constant_sim_target,
+  # realX_constant_sim_target,
   website,
   yusha_example_targets
 )
