348dfdd5b64685ea0cf98741b586d139771aa2ad
Karl Tayeb <karl.tayeb7@gmail.com>
2023-02-07 13:59:40 -0600

    add stochastic susie page

--- R/website.R
+++ R/website.R
@@ -6,5 +6,7 @@ website <- list(
   tar_render(web_home, 'notebooks/index.Rmd', output_dir='docs'),
   tar_render(web_constant_sim, 'notebooks/constant_sim_results.Rmd', output_dir='docs'),
   tar_render(web_half_normal_sim, 'notebooks/half_normal_sim_results.Rmd', output_dir='docs'),
-  tar_render(web_tilted_vs_jj, 'notebooks/tilted_vs_jj.Rmd', output_dir='docs')
+  tar_render(web_tilted_vs_jj, 'notebooks/tilted_vs_jj.Rmd', output_dir='docs'),
+  tar_render(web_stochastic_susie, 'notebooks/stochastic_susie.Rmd', output_dir='docs'),
+  tar_render(web_univariate_bayes_approximations, 'notebooks/univariate_bayes_lr_comparison.Rmd', output_dir='docs')
 )
