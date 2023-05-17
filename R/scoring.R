262db25832bee66828bb1e929e930165887c01c3
Karl Tayeb <karl.tayeb7@gmail.com>
2023-05-17 09:54:08 -0500

    redo score_cs and score_pip

--- R/scoring.R
+++ R/scoring.R
@@ -1,5 +1,7 @@
 # Scoring -------
 
+# Score CSs-------
+
 #' Check if a CS covers a particular index
 get_coverage <- function(cs, idx) {
   cs$covered <- idx %in% cs$cs
@@ -15,7 +17,15 @@ get_all_coverage <- function(css, idx) {
 
 #' compute CS and annotate with coverage info
 make_scored_cs <- function(alpha, idx){
-  cs <- logisticsusie:::get_all_cs2(alpha) %>%
+  cs <- logisticsusie::compute_cs(alpha) %>%
+    { 
+      # special case for SER
+      if(is.null(nrow(alpha))){
+        list(L1 = .)
+      } else{
+        .
+      }
+    } %>%
     get_all_coverage(idx)
   return(cs)
 }
@@ -30,44 +40,62 @@ get_lbfs <- function(fit_method, fit){
   return(lbf)
 }
 
-#' main credible set function
 score_cs <- function(fits){
-  # 1 row 1 simulation
-  tmp <- fits %>%
-    rowwise() %>%
-    mutate(fits = fit$fit) %>%
-    select(fit_method, X_name, y_name, sims, fits) %>%
-    unnest_wider(c(sims, fits)) %>%
-    unnest_longer(c(sim, fit)) %>%
-    ungroup() %>%
-    mutate(hash = purrr::map_chr(sim, rlang::hash))
-
-  # score each simulation
-  tmp2 <- tmp %>%
+  cs1 <- fits %>%
+    select(X_name, y_name, fit_method, sims, fit) %>%
+    unnest_wider(sims) %>%
+    hoist(fit,  fitted = list('fit')) %>%
+    unnest_longer(-c(X_name, y_name, fit_method)) %>%
     rowwise() %>%
     mutate(
-      cs = list(make_scored_cs(fit$alpha, sim$idx)),
-      lbf = list(get_lbfs(fit_method, fit))
+      alpha = list(fitted$alpha),
+      idx = list(sim$idx)
     ) %>%
-    select(-c(fit, sim))
-  return(tmp2)
+    ungroup() %>%
+    mutate(hash = purrr::map_chr(sim, rlang::hash)) %>%
+    select(-c(sim, fitted, fit))
+
+  cs2 <- cs1 %>%
+    rowwise() %>%
+    mutate(
+      cs = list(make_scored_cs(alpha, idx))
+    ) %>%
+    select(-c(alpha))
+  return(cs2)
 }
 
-#' extract pips and causal status of each feature
-#' return one row per simulation
+# Score PIP ------------
+
+compute_pip <- function(alpha){
+  if(is.null(nrow(alpha))){
+    return(alpha)
+  } else{
+    p <- ncol(alpha)
+    pip <- purrr::map_dbl(1:p, ~1 - prod(1 - alpha[, .x]))
+    return(pip)
+  }
+}
+
+#' Score PIPs
+#' 
+#' Function computes pips and extracts true causal status of each variable from simulaiton
+#' to be used e.g. for power/FDR plots
+#' 
+#' @return a tibble each row is a simulation, 
+#'   columns included identitying information and list-columns 
+#'   `pip` and `causal`
 score_pips <- function(fits){
   score_pip_tbl <- fits %>%
-    rowwise() %>%
-    mutate(fit = list(fit$fit[[1]])) %>%
     select(X_name, y_name, fit_method, sims, fit) %>%
-    unnest_wider(c(sims, fit)) %>%
-    unnest_longer(everything()) %>%
+    unnest_wider(sims) %>%
+    hoist(fit,  fitted = list('fit')) %>%
+    unnest_longer(-c(X_name, y_name, fit_method)) %>%
     rowwise() %>%
     mutate(
-      pip = list(logisticsusie:::get_pip(fit$alpha)),
+      pip = list(compute_pip(fitted$alpha)),
       causal = list(as.integer(1:length(pip) %in% sim$idx))
     ) %>%
-    select(-c(sim, method, fit))
+    select(-c(sim, fitted, fit))
   return(score_pip_tbl)
 }
 
